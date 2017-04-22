2cc3b31f85f3499d481fee9b1d39e443c1364d3f
DivoDivnoe <andrey-tm203@mail.ru>
2017-04-22 04:59:24 +0300

    module7 task1 is done and I dont worry about mazafaka Travis!

---
+++ js/render.js
@@ -0,0 +1,29 @@
+'use strict';
+
+window.render = function () {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+  var similarListElement = window.common.userDialog.querySelector('.setup-similar-list');
+
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    window.common.fillElement(wizardElement.querySelector('.wizard-coat'), wizard.colorCoat);
+    window.common.fillElement(wizardElement.querySelector('.wizard-eyes'), wizard.colorEyes);
+
+    return wizardElement;
+  };
+
+  return function (wizards) {
+    var fragment = document.createDocumentFragment();
+    wizards = wizards.slice();
+    similarListElement.innerHTML = '';
+
+    for (var i = 0; i < 4; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+
+    similarListElement.appendChild(fragment);
+    window.common.showElement(window.common.userDialog.querySelector('.setup-similar'), 'hidden');
+  };
+}();
